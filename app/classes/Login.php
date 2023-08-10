c01d9c1e35a6ec9decf41b0f96f9c72355354095
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2023-08-10 01:40:27 -0300

    aula 19

---
+++ app/classes/Login.php
@@ -0,0 +1,12 @@
+<?php
+
+Class Login
+{
+  public $email;
+  public $password;
+
+  public function auth()
+  {
+    return 'O meu email é '. $this->email . ' e minha senha é ' . $this->password; 
+  }
+}
